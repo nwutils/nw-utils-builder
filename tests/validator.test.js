730ccb8d17acbffaf19d30af4a156dae74da4614
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-11-29 12:22:34 -0500

    validator.log test

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -1,6 +1,31 @@
 const validator = require('../src/validator.js');
 
 describe('Validator', () => {
+  let consoleLog;
+
+  beforeEach(() => {
+    consoleLog = console.log;
+    console.log = jest.fn();
+  });
+
+  afterEach(() => {
+    console.log = consoleLog;
+  });
+
+  describe('Log', () => {
+    test('Error true', () => {
+      try {
+        validator.log('B', { global: { verbose: true } }, true);
+      } catch (error) {
+        expect(console.log)
+          .toHaveBeenCalledWith('NW-UTILS-BUILDER:');
+
+        expect(error)
+          .toEqual('B');
+      }
+    });
+  });
+
   test('Default settings snapshot', () => {
     expect(validator.settings)
       .toMatchSnapshot();
