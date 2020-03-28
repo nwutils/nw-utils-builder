fea012a6d9696f75cd9bc37d5067b66646c95854
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2020-03-28 16:43:46 -0400

    AppData tests

---
+++ tests/src/downloadNW.test.js
@@ -0,0 +1,47 @@
+const downloadNW = require('../../src/downloadNW.js');
+
+describe('downloadNW', () => {
+  describe('appData', () => {
+    const realProcess = process;
+
+    afterEach(() => {
+      global.process = realProcess;
+    });
+
+    test('Windows appData', () => {
+      global.process = Object.assign({}, realProcess, { platform: 'win32' });
+
+      const result = downloadNW.appData();
+
+      expect(result.unzips)
+        .toEqual('C:\\Users\\' + process.env.USERNAME + '\\AppData\\Local\\nw-utils\\nw-utils-builder\\nw-unzips');
+
+      expect(result.zips)
+        .toEqual('C:\\Users\\' + process.env.USERNAME + '\\AppData\\Local\\nw-utils\\nw-utils-builder\\nw-zips');
+    });
+
+    test('OSX appData', () => {
+      global.process = Object.assign({}, realProcess, { platform: 'darwin' });
+
+      const result = downloadNW.appData();
+
+      expect(result.unzips.split('\\').join('/'))
+        .toEqual('~/Library/Application Support/nw-utils/Default/nw-utils-builder/nw-unzips');
+
+      expect(result.zips.split('\\').join('/'))
+        .toEqual('~/Library/Application Support/nw-utils/Default/nw-utils-builder/nw-zips');
+    });
+
+    test('Linux appData', () => {
+      global.process = Object.assign({}, realProcess, { platform: 'asdf' });
+
+      const result = downloadNW.appData();
+
+      expect(result.unzips.split('\\').join('/'))
+        .toEqual('~/.config/nw-utils/nw-utils-builder/nw-unzips');
+
+      expect(result.zips.split('\\').join('/'))
+        .toEqual('~/.config/nw-utils/nw-utils-builder/nw-zips');
+    });
+  });
+});
